81a3d58fb798ea44deab7dca9b9673532e10cf9c
pratap798980 <116153656+pratap798980@users.noreply.github.com>
2023-03-20 22:16:11 -0700

    Create sample.java

---
+++ sample.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyServlet extends HttpServlet {
+
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    response.setContentType("text/html");
+    response.setStatus(HttpServletResponse.SC_OK);
+    response.getWriter().println("<html><body>Hello World!</body></html>");
+  }
+}
